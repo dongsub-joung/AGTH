50353a645e5be868c8e555721f76b24922fb4f10
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-10-22 00:04:06 +0900

    IDK WTF

---
+++ aGTH/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    // @todo system procedure
+    // .exe -> memory
+    // https://www.codeproject.com/Articles/13323/Intercepting-WinAPI-calls
+
+    // a program dll runtime(windows System Procedures call while runtime)
+    // https://drmemory.org/page_drstrace.html
+
+    // decoding UTF-8
+    
+    // print with GUI
+
+    // the lastest: runtime
+    // https://github.com/emilk/egui
+    // let app;
+    // app.bulild()
+}
